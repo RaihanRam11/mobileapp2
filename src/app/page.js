957bef8d8a538d7983676ff2dcf29eb8e5c0006a
raihanram11 <raihanram11.raihanramdhani451@gmail.com>
2024-09-23 20:44:00 +0700

    Membuat Page Home

---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+export default function Home() {
+  return (
+    <>
+      <h2 className="text-Left text-3xl w-full">Hello Guys !!</h2>
+      <div>
+        <p className="text-Left text-2xl w-full	">My Name Is Raihan Ramdhani </p>
+      </div>
+
+      <div className="flex mt-16"></div>
+        <div className="md:w-3/4 px-4 "></div>
+          <div className=" bg-white p-10  rounded-xl">
+          <h3 className="text-3xl py-3">
+            My name is Raihan Ramdhani From The Informatics Management Study Program 
+          </h3>
+        </div>
+    </>
+  );
+}
