cd0e8ac6986d4717de6fdbce37e466b3ff4f4833
raihanram11 <raihanram11.raihanramdhani451@gmail.com>
2024-09-23 20:57:12 +0700

    Part 3

--- src/app/page.js
+++ src/app/page.js
@@ -9,14 +9,6 @@ export default function Home() {
       <div>
         <p className="text-Left text-2xl w-full	">My Name Is Raihan Ramdhani </p>
       </div>
-
-      <div className="flex mt-16"></div>
-        <div className="md:w-3/4 px-4 "></div>
-          <div className=" bg-white p-10  rounded-xl">
-          <h3 className="text-3xl py-3">
-            My name is Raihan Ramdhani From The Informatics Management Study Program 
-          </h3>
-        </div>
     </>
   );
 }
