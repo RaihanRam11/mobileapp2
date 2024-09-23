2a1c060ff20ce819d166a8cbde5f6984e3926af7
raihanram11 <raihanram11.raihanramdhani451@gmail.com>
2024-09-23 21:02:13 +0700

    part 5

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <div class="text-5xl font-extrabold ...">
+      <div class="text-5xl font-extrabold  mt-24">
         <span class="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
             Hello Guys
         </span>
