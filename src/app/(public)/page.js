0efbe34724883cd55bd9e94364a4dc33d167971e
Raihan.Ram11 <raihanramdhani451@gmail.com>
2024-11-26 20:35:43 +0700

    nama

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -31,7 +31,7 @@ export default function Dashboard() {
         <div className='flex w-full bg-white rounded-xl '>
           <div className="flex-1 p-10">
             <div className="font-extrabold text-[48px] mt-10">
-              <div>I&lsquo;m Rayan Adlrdard</div>
+              <div>I&lsquo;Raihan</div>
               <div>
                 <span className="text-[#FFB400]">Front-end</span> Developer 
               </div>
