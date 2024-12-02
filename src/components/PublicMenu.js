15d6d5b034de14991ae57cfbdde155e817b65062
Raihan.Ram11 <raihanramdhani451@gmail.com>
2024-12-02 21:11:19 +0700

    Pembaruan

--- src/components/PublicMenu.js
+++ src/components/PublicMenu.js
@@ -15,7 +15,7 @@ export default function PublicMenu() {
             <div className="max-w-5xl mx-auto px-6 md:px-12 xl:px-6">
               <div className="flex flex-wrap items-center justify-between">
                 <div>
-                  <a href="/">Rayan Adlrdard</a>
+                  <a href="/">Raihan Ramdhani</a>
                 </div>
                 <div>
                   <ul className="flex flex-row gap-6">
@@ -24,6 +24,9 @@ export default function PublicMenu() {
                     </li> */}
                     <li className="mt-1">
                       <a className="btn-link" href="/work">Work</a>
+                    </li>
+                    <li className="mt-1">
+                      <a className="btn-link" href="/blogs">Blogs</a>
                     </li>
                     <li className="mt-1">
                       <a className="btn-link" href="/contact">Contact</a>
